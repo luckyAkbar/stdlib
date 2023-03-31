a9d48b33d184a362ae38b581e8c2e0b31112e4db
lucky akbar <luckyakbar1509@gmail.com>
2023-03-31 21:09:42 +0700

    feat: new helper function: Dump and WrapCloser

--- helper/helper.go
+++ helper/helper.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 
 	"github.com/google/uuid"
+	"github.com/kumparan/go-utils"
+	"github.com/sirupsen/logrus"
 )
 
 // GenerateID generates a random ID using "github.com/google/uuid"
@@ -12,4 +14,16 @@ import (
 func GenerateID() string {
 	id := uuid.New()
 	return strings.ReplaceAll(id.String(), "-", "")
+}
+
+// Dump to json using json marshal. wrapper for "github.com/kumparan/go-utils".Dump func
+func Dump(i interface{}) string {
+	return utils.Dump(i)
+}
+
+// WrapCloser wrap closer. If closer return error, log the error
+func WrapCloser(closeFn func() error) {
+	if err := closeFn(); err != nil {
+		logrus.Error(err)
+	}
 }
