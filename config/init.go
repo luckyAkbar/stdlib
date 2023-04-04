5e956ff0af43ea810f1995ffa3f17f326695bf10
lucky akbar <luckyakbar1509@gmail.com>
2023-04-05 06:30:24 +0700

    feat: init config file reading

---
+++ config/init.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("../")
+	viper.AddConfigPath("../../")
+	viper.AddConfigPath("../../../")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		logrus.Error(err)
+		panic("failed to read config file")
+	}
+}
