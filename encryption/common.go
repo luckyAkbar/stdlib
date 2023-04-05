49b2f35f79ebbe0ce7fd7209fd8116bd672c0e56
lucky akbar <luckyakbar1509@gmail.com>
2023-04-05 23:46:37 +0700

    feat: aes key length

--- encryption/common.go
+++ encryption/common.go
@@ -2,6 +2,14 @@ package encryption
 
 import "strings"
 
+type AESKeyLength int
+
+const (
+	AES128 AESKeyLength = 16
+	AES192 AESKeyLength = 24
+	AES256 AESKeyLength = 32
+)
+
 // ParseTestKey is a helper function to parse a test key to avoid test key detected as real key
 func ParseTestKey(key string) string {
 	return strings.ReplaceAll(key, "TESTING KEY", "PRIVATE KEY")
