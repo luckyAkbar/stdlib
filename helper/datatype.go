bb94ff91d50b290c33f732e89c082c56f22a69fd
lucky akbar <luckyakbar1509@gmail.com>
2023-04-16 20:30:05 +0700

    feat: helper to read file metadata

---
+++ helper/datatype.go
@@ -0,0 +1,8 @@
+package helper
+
+// FileMetadata is a struct that contains file metadata
+type FileMetadata struct {
+	Name        string
+	Size        int64
+	ContentType string
+}
--- helper/helper.go
+++ helper/helper.go
@@ -4,6 +4,7 @@ package helper
 import (
 	"io"
 	"mime/multipart"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -56,6 +57,26 @@ func MultipartFileSaver(file *multipart.FileHeader, path string) error {
 	}
 
 	return nil
+}
+
+// ReadFileMetadata read file metadata from multipart file header
+func ReadFileMetadata(file *multipart.FileHeader) (*FileMetadata, error) {
+	f, err := file.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	buffer := make([]byte, 512)
+	_, err = f.Read(buffer)
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+
+	return &FileMetadata{
+		Name:        file.Filename,
+		Size:        file.Size,
+		ContentType: http.DetectContentType(buffer),
+	}, nil
 }
 
 // DeleteFile wrapper for os.Remove. will delete file from given path
