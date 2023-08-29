26f53997a35dd49bf010ecc1b17a758e99ab7865
lucky akbar <luckyakbar1509@gmail.com>
2023-08-29 10:23:31 +0700

    fix: returning the downloaded filename

--- helper/download_scraper_meme_media.go
+++ helper/download_scraper_meme_media.go
@@ -11,11 +11,12 @@ import (
 )
 
 // DownloadMemeScrapingResultMedia is a function to download meme media from scraping result. For now, works for both meme
-// sourced from 9gag.com and 1cak.com
-func DownloadMemeScrapingResultMedia(ctx context.Context, url, outputPath, referer string) error {
+// sourced from 9gag.com and 1cak.com.
+// Returning the filename of the downloaded media.
+func DownloadMemeScrapingResultMedia(ctx context.Context, url, outputPath, referer string) (string, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0")
@@ -24,37 +25,37 @@ func DownloadMemeScrapingResultMedia(ctx context.Context, url, outputPath, refer
 	client := http.DefaultClient
 	resp, err := client.Do(req)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	defer WrapCloser(resp.Body.Close)
 
 	if resp.StatusCode != http.StatusOK {
-		return errors.New("9gag media returning non 200")
+		return "", errors.New("9gag media returning non 200")
 	}
 
 	err = os.MkdirAll(outputPath, os.ModePerm)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	filename := outputPath + GenerateID()
 
 	outfile, err := os.Create(filename)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	_, err = io.Copy(outfile, resp.Body)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	mime, err := mimetype.DetectFile(filename)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	newFilename := filename + mime.Extension()
-	return os.Rename(filename, newFilename)
+	return newFilename, os.Rename(filename, newFilename)
 }
